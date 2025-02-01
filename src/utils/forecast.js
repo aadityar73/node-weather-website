0b437bc12b9e22e4f496bc8881380c844631f855
Aaditya Raikar <aadityaraikar73@gmail.com>
2025-02-01 16:32:06 +0530

    Corrected environment variable reference

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `https://api.weatherstack.com/current?access_key=${process.env.FORECAST_ENV}&query=${latitude},${longitude}`;
+  const url = `https://api.weatherstack.com/current?access_key=${process.env.FORECAST_API}&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
